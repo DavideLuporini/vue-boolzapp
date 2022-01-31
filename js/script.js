5d60d0a38f09b7c417b25b8951a518f8c69b639f
illupovero <davideluporini1996@gmail.com>
2022-01-31 20:51:17 +0100

    ended milestone 1

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
+console.log('hi')
+
 var app = new Vue({
     el: '#app',
-    user: {
-        name: 'Nome Utente',
-        avatar: '_io'
+    data: {
+
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+        contacts: [{
+                name: 'Michele',
+                avatar: '_1',
+                message: 'butta la pasta',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+                message: 'butta la pasta',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                message: 'butta la pasta',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                message: 'butta la pasta',
+            },
+        ],
+
     },
-    contacts: [{
-            name: 'Michele',
-            avatar: '_1',
-        },
-        {
-            name: 'Fabio',
-            avatar: '_2',
-        },
-        {
-            name: 'Samuele',
-            avatar: '_3',
-        },
-        {
-            name: 'Luisa',
-            avatar: '_4',
-        },
-    ]
 })
