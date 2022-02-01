64dafc6aa2194fe175b087619c626b692177a35c
illupovero <davideluporini1996@gmail.com>
2022-02-01 02:18:25 +0100

    added dinamic date to messages

--- js/script.js
+++ js/script.js
@@ -5,30 +5,43 @@ var app = new Vue({
     data: {
 
         user: {
-            name: 'Nome Utente',
-            avatar: '_io'
+            name: 'Davide',
+            avatar: '_io',
+            message: 'io sono tuo padre',
         },
         contacts: [{
-                name: 'Michele',
+                name: 'Marco',
                 avatar: '_1',
                 message: 'butta la pasta',
             },
             {
-                name: 'Fabio',
+                name: 'Andrea',
                 avatar: '_2',
                 message: 'sono incinto',
             },
             {
-                name: 'Samuele',
+                name: 'federico',
                 avatar: '_3',
                 message: 'ti va di uscire con me?',
             },
             {
-                name: 'Luisa',
+                name: 'Giorgia',
                 avatar: '_4',
-                message: 'da oggi sono luiso',
+                message: 'da oggi sono Giorgio',
             },
         ],
-
+    },
+    methods: {
+        getDate() {
+            const currentDate = new Date().toLocaleDateString();
+            return currentDate;
+        },
+        getHour() {
+            const hours = new Date().getHours();
+            const minutes = new Date().getMinutes();
+            const seconds = new Date().getSeconds();
+            const currentTime = `${hours}:${minutes}:${seconds}`;
+            return currentTime;
+        },
     },
 })
