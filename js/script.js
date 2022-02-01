9188d6a56804137c7ab9000470b498d17a9e51e9
illupovero <davideluporini1996@gmail.com>
2022-02-01 15:51:09 +0100

    added active function

--- js/script.js
+++ js/script.js
@@ -4,49 +4,197 @@ var app = new Vue({
     el: '#app',
     data: {
 
+        currentContact: 0,
+
         user: {
             name: 'Davide',
-            avatar: '_io',
-            message: 'io sono tuo padre',
+            avatar: '_io'
         },
         contacts: [{
-                name: 'Marco',
+                name: 'Michele',
                 avatar: '_1',
-                message: 'butta la pasta',
+                visible: true,
+                messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                name: 'Andrea',
+                name: 'Fabio',
                 avatar: '_2',
-                message: 'sono incinto',
+                visible: true,
+                messages: [{
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                name: 'federico',
+                name: 'Samuele',
                 avatar: '_3',
-                message: 'ti va di uscire con me?',
+                visible: true,
+                messages: [{
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                name: 'Giorgia',
+                name: 'Luisa',
                 avatar: '_4',
-                message: 'da oggi sono Giorgio',
+                visible: true,
+                messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                name: 'Giorgia',
+                name: 'Luisa',
                 avatar: '_4',
-                message: 'da oggi sono Giorgio',
+                visible: true,
+                messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                name: 'Giorgia',
+                name: 'Luisa',
                 avatar: '_4',
-                message: 'da oggi sono Giorgio',
+                visible: true,
+                messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
             },
             {
-                name: 'Giorgia',
+                name: 'Luisa',
                 avatar: '_4',
-                message: 'da oggi sono Giorgio',
+                visible: true,
+                messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                visible: true,
+                messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                visible: true,
+                messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                visible: true,
+                messages: [{
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
             },
         ],
     },
     methods: {
+        isActive(index) {
+            return index === this.currentContact
+        },
+        actualContact(index) {
+            this.currentContact = index;
+        },
+
         getDate() {
             const currentDate = new Date().toLocaleDateString();
             return currentDate;
@@ -59,4 +207,5 @@ var app = new Vue({
             return currentTime;
         },
     },
+
 })
